93dc1ccba5733162c552855ab75d161aaecdd94e
Milo Gertjejansen <milo@articulate.com>
2020-02-21 09:46:15 -0600

    Move to static html page

--- web.py
+++ web.py
@@ -3,29 +3,16 @@
 from flask import Flask, request, redirect, Response
 from flask import render_template as rt
 from generator import generator
+
 app = Flask(__name__)
 
-@app.route('/', defaults={'username': ''})
-@app.route('/<username>')
-def index(username):
-    return rt(
-        'index.html',
-        username=username,
-        link=request.url_root + "rss/" + username
-    )
 
-@app.route('/between', methods=['POST'])
-def between():
-    return redirect('/' + request.form['username'])
-
-@app.route('/rss', defaults={'username':''})
-@app.route('/rss/<username>')
+@app.route("/rss", defaults={"username": ""})
+@app.route("/rss/<username>")
 def rss(username):
-    print(username)
     if not username:
-        return redirect('/')
+        return redirect("/")
 
     res = Response(generator.Generator(username, link=request.url).generate())
-    res.headers['Content-Type'] = 'application/xml'
+    res.headers["Content-Type"] = "application/xml"
     return res
-
